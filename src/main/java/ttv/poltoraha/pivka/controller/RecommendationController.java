e00647d824e1f1eaf6042ff196f6e0d03f2756de
JustBeginnerAtIt <tarabukin99@icloud.com>
2025-06-08 09:00:31 +0800

    PIVO-111: Added controller

--- src/main/java/ttv/poltoraha/pivka/controller/RecommendationController.java
+++ src/main/java/ttv/poltoraha/pivka/controller/RecommendationController.java
@@ -1,6 +1,8 @@
 package ttv.poltoraha.pivka.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import ttv.poltoraha.pivka.entity.Author;
 import ttv.poltoraha.pivka.entity.Book;
@@ -13,7 +15,7 @@ import java.util.List;
 @RequestMapping("/recommend")
 @RequiredArgsConstructor
 public class RecommendationController {
-
+    private final Logger logger = LoggerFactory.getLogger(AuthorController.class);
     private final RecommendationService recommendationService;
 
     @GetMapping("/authors")
