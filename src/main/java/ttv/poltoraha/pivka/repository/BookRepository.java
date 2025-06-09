25b6d56304b3f54e681c5efb33eb13090b660082
JustBeginnerAtIt <tarabukin99@icloud.com>
2025-06-10 01:23:44 +0800

    PIVO-201: Added JPQL request to BookRepository

--- src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
+++ src/main/java/ttv/poltoraha/pivka/repository/BookRepository.java
@@ -1,10 +1,34 @@
 package ttv.poltoraha.pivka.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ttv.poltoraha.pivka.entity.Book;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
+    // JPQL вариант
+    @Query(value = """
+       SELECT b FROM book b
+       WHERE b.author.fullName = :fullName
+       AND b.author.avgRating = (
+           SELECT MAX(a.avgRating) FROM author a WHERE a.fullName = :fullName
+           )
+    """)
+    List<Book> findBooksByAuthorFullNameAndRating(@Param("fullName") String fullName);
 
+    // Второй вариант через SQL
+//    @Query(value = """
+//       SELECT b.*
+//       FROM book b
+//       JOIN author a ON b.author_id = a.id
+//       WHERE a.full_name = :fullName
+//       AND a.rating = (
+//           SELECT MAX(a2.rating) FROM author a2 WHERE a2.full_name = :fullName
+//           )
+//    """, nativeQuery = true)
+//    List<Book> findBooksByAuthorFullNameAndRating(@Param("fullName") String fullName);
 }
